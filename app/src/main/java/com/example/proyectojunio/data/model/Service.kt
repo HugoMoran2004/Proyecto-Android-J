28bae30966e63d68b65d50dfeb66cadc8247eddf
HugoMoran2004 <hugomoran2004@gmail.com>
2025-05-20 19:40:27 +0200

    Retro fit hecho

--- app/src/main/java/com/example/proyectojunio/data/model/Service.kt
+++ app/src/main/java/com/example/proyectojunio/data/model/Service.kt
@@ -6,5 +6,5 @@ import retrofit2.http.Query
 
 interface Service{
     @GET("trending/tv/day?language=en-US")
-    fun listSeries(@Query("apiKey")apiKey : String): SerieResult
+   suspend fun listSeries(@Query("api_key")apiKey : String): SerieResult
 }
